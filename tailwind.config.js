55339a0f63bb44640aa339c540ae995302ace9a5
Pijuli <marcs14@gmail.com>
2020-05-23 01:27:59 +0200

    Fix navigation mode. Add css purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './public/**/*.html',
+      './src/**/*.vue',
+    ],
+  },
   theme: {
     extend: {},
   },
