ee6d7f6c90b2474202c1642a7e85963db2c27b33
Pijuli <marcs14@gmail.com>
2020-05-29 21:05:23 +0200

    Maximized app if production build

--- src/background.js
+++ src/background.js
@@ -16,7 +16,8 @@ protocol.registerSchemesAsPrivileged([{scheme: 'app', privileges: { secure: true
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600, webPreferences: {
+  const fullScreen = isDevelopment ? false : true
+  win = new BrowserWindow({ width: 800, height: 600, fullscreen: fullScreen, webPreferences: {
     nodeIntegration: true,
   } })
 
